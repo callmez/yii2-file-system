a4b4df7ce6c310fdbfbcfe200bdc8feb9ffd5f90
callmez <callme-z@qq.com>
2014-11-13 10:42:05 +0800

    去除Local的 的Yii别名支持

--- adapters/Local.php
+++ adapters/Local.php
@@ -1,13 +1,8 @@
 <?php
 namespace callmez\file\system\adapters;
 
-use Yii;
 use League\Flysystem\Adapter\Local as LocalFileSystem;
 
 class Local extends LocalFileSystem
 {
-    public function __construct($root)
-    {
-        parent::__construct(Yii::getAlias($root));
-    }
 }
